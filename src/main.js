34850e711d5cdfba6f7734624353841c4df39169
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-04 21:44:16 +0300

    fixedbag

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import './js/menu.js';
 import './js/reviews-slider.js';
-// import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
+import 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
