b5a26b36c8e6c935dc288f7c49bca191f770d074
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-04 21:41:32 +0300

    fixedbag

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -1,24 +1,24 @@
-// const swiper = new Swiper('.swiper-container', {
-//   loop: true,
-//   slidesPerView: 1,
-//   spaceBetween: 0,
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 0,
-//     },
-//     1280: {
-//       slidesPerView: 3,
-//       spaceBetween: 0,
-//     },
-//   },
-//   pagination: {
-//     el: '.pagination',
-//     bulletClass: 'pagination__button',
-//     bulletActiveClass: 'pagination__button--active',
-//   },
-//   navigation: {
-//     nextEl: '.carousel-button.next',
-//     prevEl: '.carousel-button.prev',
-//   },
-// });
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button--active',
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+});
