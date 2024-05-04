f925cf74434960ac8b7ef502a555f5a67f8f7201
Dima_Ostapchuk <Adrenalinbd@gmail.com>
2024-05-05 01:38:33 +0300

    fixed swiper js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import './js/menu.js';
 import './js/reviews-slider.js';
 import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+});
