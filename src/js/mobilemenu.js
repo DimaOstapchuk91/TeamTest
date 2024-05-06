42fe22004a440f8563792158565d2c1e60dad8c9
ReaperDal <reaper_dal@knu.ua>
2024-05-06 12:15:44 +0300

    Update mobilemenu.js

--- src/js/mobilemenu.js
+++ src/js/mobilemenu.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var anchorLinks = document.querySelectorAll('.mob-link');
 
   anchorLinks.forEach(function(link) {
-    link.addEventListener('click', function() {
       
       var mobileMenu = document.querySelector('.backdrop');
       mobileMenu.style.display = 'none';
